32bc71d32c672166fb252602cd453eeedd26b0d4
Kit Hensel <kithensel@gmail.com>
2016-07-01 16:09:13 -0700

    format side-nav

--- build/tasks/watch.js
+++ build/tasks/watch.js
@@ -11,7 +11,7 @@ var paths = require('../paths');
 var htmlTasks = [ 'html', browserSync.reload ];
 var templateTasks = [ 'templates', 'js', 'html', browserSync.reload ];
 var jsTasks = [ 'js', browserSync.reload ];
-var cssTasks = [ 'css' ];
+var cssTasks = [ 'css', browserSync.reload ];
 
 function interceptErrors(error) {
   var args = Array.prototype.slice.call(arguments);
