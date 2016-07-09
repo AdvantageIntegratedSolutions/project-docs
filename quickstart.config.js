25f343fadaa69e793d3540ae945b224eb23d1d84
Kit Hensel <kithensel@gmail.com>
2016-07-09 14:37:31 -0700

    remove quickstart.config

--- quickstart.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "project-docs",
-  description: "",
-  client: "Advantage",
-  origin: "https://github.com/AdvantageIntegratedSolutions/quickstart.git",
-  authors: ["khensel@advantagequickbase.com"],
-  bootstrap: "./app/main.js",
-  timezone: "mountain"
-}
