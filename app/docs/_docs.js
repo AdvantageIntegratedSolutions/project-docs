8cd33b6c2374f6ef0d178938b1a6d4a03c7afdff
Kit Hensel <kithensel@gmail.com>
2016-07-09 13:28:45 -0700

    add classes on hover to dashboard

--- app/docs/_docs.js
+++ app/docs/_docs.js
@@ -1,39 +1,15 @@
 const projects = [
 	{ 
-		name: "project-docs", 
-		description: "Platform to develop and store documentation for common libraries, projects and tools"
-	},
-	{ 
 		name: "base-js", 
 		description: "JavaScript library built for the QuickBase API."
 	},
 	{ 
+		name: "project-docs", 
+		description: "Platform to develop and store documentation for common libraries, projects and tools."
+	},
+	{ 
 		name: "quickstart", 
-		description: "JavaScript library built for the QuickBase API."
-	},
-		{ 
-		name: "test", 
-		description: "JavaScript library built for the QuickBase API."
-	},
-		{ 
-		name: "test1", 
-		description: "JavaScript library built for the QuickBase API."
-	},
-		{ 
-		name: "test2", 
-		description: "JavaScript library built for the QuickBase API."
-	},
-		{ 
-		name: "test3", 
-		description: "JavaScript library built for the QuickBase API."
-	},
-		{ 
-		name: "test4", 
-		description: "JavaScript library built for the QuickBase API."
-	},
-		{ 
-		name: "test5", 
-		description: "JavaScript library built for the QuickBase API."
+		description: "Angular environment for quickbase."
 	}
 ]
 
--- app/project/projects.component.js
+++ app/project/projects.component.js
@@ -4,12 +4,14 @@ class ProjectCtrl {
   constructor($scope, $rootScope) {
   	this.projects = docs;
   	this.addColor();
+
+    this.hover = false;
   }
 
   addColor(){
-  	const rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "purple"];
+  	const rainbow = ["blue darken-2", "green darken-1", "gray-bar"];
   	this.projects.forEach(function(project, index){
-  		project["color"] = rainbow[ index % 7 ];
+  		project["color"] = rainbow[ index % rainbow.length ];
   	});
   }
 }
