800097be72045c10835d28149bfd7ffad158822e
Kit Hensel <kithensel@gmail.com>
2016-07-09 17:06:10 -0700

    remove dist from gitignore

--- build/tasks/deploy.js
+++ build/tasks/deploy.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
+
+var tasks = [ 'html', 'templates', 'js', 'css' ];
  
-gulp.task('deploy', function() {
+gulp.task('deploy', tasks, function() {
   return gulp.src('./dist/**/*')
     .pipe(ghPages());
 });
