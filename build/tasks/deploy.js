566ffe2d8c14fcfcb3fdb2b0b43e0f7b552eeb49
Kit Hensel <kithensel@gmail.com>
2016-07-12 13:24:40 -0700

    remove gh-pages

--- build/tasks/deploy.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var ghPages = require('gulp-gh-pages');
-
-var tasks = [ 'html', 'templates', 'js', 'css' ];
- 
-gulp.task('deploy', tasks, function() {
-  return gulp.src('./dist/**/*')
-    .pipe(ghPages());
-});
