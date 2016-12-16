5c572758acc3713e8b4ce0b0547e8cba62b9213d
Kit Hensel <kithensel@gmail.com>
2016-12-16 11:37:29 -0800

    remove process library

--- app/docs/index.js
+++ app/docs/index.js
@@ -2,50 +2,42 @@ const projects = [
 	{ 
 		name: "base-js", 
 		description: "JavaScript library built for the QuickBase API.",
-		status: 'current',
-		type: 'project'
+		status: 'current'
 	},
 	{ 
 		name: "project-docs", 
 		description: "Platform to develop and store documentation for common libraries, projects and tools.",
-		status: 'current',
-		type: 'project'
+		status: 'current'
 	},
 	{ 
 		name: "pdf-link", 
 		description: "Product to convert HTML pages to pdf's and save pdf's to QuickBase.",
-		status: 'current',
-		type: 'project'
+		status: 'current'
 	},
 	{ 
 		name: "isomorph-js", 
 		description: "AWS Lambda that executes client-side QuickBase code.",
-		status: 'current',
-		type: 'project'
+		status: 'current'
 	},
 	{ 
 		name: "quickstart-angular", 
 		description: "Angular environment for QuickBase.",
-		status: 'in progress',
-		type: 'project'
+		status: 'in progress'
 	},	
 	{ 
 		name: "server-checklist", 
 		description: "Documentation for server maintainance and Web app uptime monitoring.",
-		status: 'in-progress',
-		type: 'process'
+		status: 'in-progress'
 	},
 	{ 
 		name: "quickstart-portal", 
 		description: "Client side-portal utilizing AWS Lambda for security/validation.",
-		status: 'not started',
-		type: 'project'
+		status: 'not started'
 	},
 	{ 
 		name: "code-snippets", 
 		description: "Simple reusable code snippets or solutions.",
-		status: 'current',
-		type: 'project'
+		status: 'current'
 	}
 ]
 
