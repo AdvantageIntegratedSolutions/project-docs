3eff1e3cfce690c76a9c1cf9277869d1dc6e44d9
Jimmy Stafford <j.stafford1992@gmail.com>
2017-06-21 10:54:07 -0600

    Form Engine Client Setup Docs
    First run through of the documentation following the steps Matt walked
    through with me. Covering quick base and the Codebase updates.

--- app/docs/index.js
+++ app/docs/index.js
@@ -1,52 +1,57 @@
 const projects = [
-	{ 
-		name: "base-js", 
+	{
+		name: "base-js",
 		description: "JavaScript library built for the QuickBase API.",
 		status: 'current'
 	},
-	{ 
-		name: "project-docs", 
+	{
+		name: "project-docs",
 		description: "Platform to develop and store documentation for common libraries, projects and tools.",
 		status: 'current'
 	},
-	{ 
-		name: "pdf-link", 
+	{
+		name: "pdf-link",
 		description: "Product to convert HTML pages to pdf's and save pdf's to QuickBase.",
 		status: 'current'
 	},
-	{ 
-		name: "isomorph-js", 
+	{
+		name: "isomorph-js",
 		description: "AWS Lambda that executes client-side QuickBase code.",
 		status: 'current'
 	},
-	{ 
-		name: "quickstart-angular", 
+	{
+		name: "quickstart-angular",
 		description: "Angular environment for QuickBase.",
 		status: 'in progress'
-	},	
-	{ 
-		name: "server-checklist", 
+	},
+	{
+		name: "server-checklist",
 		description: "Documentation for server maintainance and Web app uptime monitoring.",
 		status: 'in-progress'
 	},
-	{ 
-		name: "quickstart-portal", 
+	{
+		name: "quickstart-portal",
 		description: "Client side-portal utilizing AWS Lambda for security/validation.",
 		status: 'not started'
 	},
-	{ 
-		name: "quickbase-snippets", 
+	{
+		name: "quickbase-snippets",
 		description: "Simple reusable code snippets or solutions.",
 		status: 'current'
 	},
-	{ 
-		name: "coke-calendar", 
+	{
+		name: "coke-calendar",
 		description: "JavaScript monthly/weekly/daily asset calendar.",
 		status: 'current'
 	},
 	{
 		name: "QPI",
 		description: "Exact forms, easier - Format data, build tables, and go from database to Web page with minimal effort",
+		status: "current"
+	},
+	{
+		name: "form-engine-client-setup",
+		description: "Instructions for creating new Form Engine instance and setup for new client",
 		status: "current"
 	}
 ]
